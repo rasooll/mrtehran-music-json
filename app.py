9e399fc5e1d44cb97f42dc07d3d2f325d839167f
Rasool Safari <safari@rasoll.com>
2020-05-28 19:03:56 +0430

    Cleanup and edit the main application.

--- app.py
+++ app.py
@@ -1,17 +1,22 @@
 import requests
 import json
+import os
 from bs4 import BeautifulSoup
 
 
-def get_featured(pages=1):
+def get_musics(pages=1, musictype="featured"):
     musics = []
-    featured_url = "https://mrtehran.com/browse/featured/page-{}"
-    for page_number in range(1,(pages+1)):
+    url = "https://mrtehran.com/browse/{musictype}/page-{pages}"
+    for page_number in range(1, (pages+1)):
 
-        page = requests.get(featured_url.format(page_number))
+        page = requests.get(url.format(
+            musictype=musictype,
+            pages=page_number
+        ))
         soup = BeautifulSoup(page.content, 'html.parser')
 
-        featured = soup.find_all('div', attrs={'class': 'row musicbox-related'})
+        featured = soup.find_all(
+            'div', attrs={'class': 'row musicbox-related'})
         for item in featured:
             for song in item.find_all('div', attrs={'class': 'musicbox-item'}):
                 musics.append({
@@ -22,6 +27,12 @@ def get_featured(pages=1):
                 })
 
     return musics
+try:
+    os.mkdir("_deploy")
+except FileExistsError:
+    pass
 
-with open('_deploy/featured.json', 'w') as featured:
-    featured.write(json.dumps(get_featured(), indent=2))
+for music_type in ["featured","latest","popular"]:
+    with open('_deploy/{}.json'.format(music_type), 'w') as featured:
+        featured.write(json.dumps(get_musics(1,music_type), indent=2))
+
